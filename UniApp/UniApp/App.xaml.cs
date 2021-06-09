d9bb77ecbbec225c6fb76badf49c435828009539
Ben <benjaminvonsnarski@gmail.com>
2021-06-09 18:44:23 +1000

    .

--- UniApp/UniApp/App.xaml.cs
+++ UniApp/UniApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace UniApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
